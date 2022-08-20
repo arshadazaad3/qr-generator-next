37694af6b4b7c0058994ae1381feaa9d9844af3d
arshadazaad3 <arshad.azaad32@gmail.com>
2022-08-20 20:42:19 +0800

    Updated Title

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>QR Planet</title>
+        <title>QR World</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
