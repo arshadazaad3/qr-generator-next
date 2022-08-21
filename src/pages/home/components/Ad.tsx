39e55b1851f4eb2099616f13e1606337f9a454e3
arshadazaad3 <arshad.azaad32@gmail.com>
2022-08-21 21:48:19 +0800

    Adsense Integration

--- src/pages/home/components/Ad.tsx
+++ src/pages/home/components/Ad.tsx
@@ -14,13 +14,14 @@ function Ad() {
         animate={{ scale: 1, opacity: 1 }}
         exit={{ scale: 0.8, opacity: 0 }}
         transition={{ type: 'spring', duration: 1.5 }}
-        className="rounded-3xl h-full p-3 justify-center flex content-center items-center flex-col space-y-10 w-full"
+        className="rounded-3xl h-full p-3  flex  flex-col  w-full text-center"
         style={{
           backgroundColor: '#c5e3f6',
           minHeight: 700,
           minWidth: 300,
         }}
       >
+        <div>Ads</div>
         <div>
           <ins
             className="adsbygoogle"
